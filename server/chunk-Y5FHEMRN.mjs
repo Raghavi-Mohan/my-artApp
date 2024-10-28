ce3b3ed0423f9b0f1cb3052f507ae00b7594f155
Raghavi Mohan <70186244+Raghavi-Mohan@users.noreply.github.com>
2024-10-28 08:51:17 -0400

    updating main
    updating main

---
+++ server/chunk-Y5FHEMRN.mjs
@@ -0,0 +1,291 @@
+import './polyfills.server.mjs';
+import{A as v,B as H,C as R,D as T,G as L,K as U,N as g,O,a as p,b as l,c as d,d as I,e as h,f as C,g as j,h as F,i as c,j as B,k as b,l as s,m as t,n as i,o as a,p as r,q as x,r as D,s as z}from"./chunk-ZV3RRGW4.mjs";function G(e,o){if(e&1&&(t(0,"div",6),a(1,"img",7),t(2,"p"),r(3),i()()),e&2){let n=o.$implicit;c(),s("src",n.url,j)("alt",n.name),c(2),x(n.name)}}var w=class e{images=[{name:"Gopuram",url:"Budha.png"},{name:"babyKrishna",url:"babyKrishna.png"},{name:"homeSweetHome",url:"homeSweetHome.png"},{name:"Budha",url:"Budha.png"},{name:"girlWithGuitar",url:"girlAndGuitar.png"}];static \u0275fac=function(n){return new(n||e)};static \u0275cmp=l({type:e,selectors:[["app-catalogcomponent"]],decls:11,vars:1,consts:[[1,"description"],[1,"gallery"],["class","image-item",4,"ngFor","ngForOf"],[1,"home"],["routerLink","/"],["routerLink","/page2"],[1,"image-item"],[3,"src","alt"]],template:function(n,u){n&1&&(t(0,"body")(1,"div",0)(2,"p"),r(3,"Discover how we blend traditional art with modern technology to create stunning, interactive experiences. Join us on this journey to inspire and unleash your imagination through the fusion of color and code!"),i()(),t(4,"div",1),b(5,G,4,3,"div",2),i(),t(6,"div",3)(7,"a",4),r(8,"Home"),i(),t(9,"a",5),r(10,"Explore making videos"),i()()()),n&2&&(c(5),s("ngForOf",u.images))},dependencies:[v,g],styles:[".gallery[_ngcontent-%COMP%]{display:flex;flex-wrap:wrap;gap:15px}.image-item[_ngcontent-%COMP%]{text-align:center;width:200px;border-radius:5px;border-style:double;border-color:#e7caab;border-width:10px}body[_ngcontent-%COMP%]{background-image:linear-gradient(to right,#e9e8e6,#e7caab)}img[_ngcontent-%COMP%]{max-width:100%;border-radius:5px}",`.home[_ngcontent-%COMP%] {
+      max-width: 5.2rem;
+      max-width: 1200px;
+      justify-content: center;
+      display: grid;
+      align-items: center;
+      float: center;
+      margin-top: 100px;
+      margin-bottom: 30px;
+      background-image: linear-gradient(to left,#e6d9ca,#E7CAAB);
+    }
+    .description[_ngcontent-%COMP%] {
+      max-width: 5.2rem;
+      max-width: 1200px;
+      justify-content: center;
+      display: grid;
+      align-items: center;
+      float: center;
+      margin-top: 50px;
+      margin-bottom: 50px;
+      background-image: linear-gradient(to left,#e4dbd0,#E7CAAB);
+    }
+    p[_ngcontent-%COMP%]{
+    font-size: 18px; 
+
+    background: linear-gradient(to right, #f9611a, #f40b93, #8d029c);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    color: transparent;   
+ }
+    a[_ngcontent-%COMP%] {
+      max-width: 5.2rem;
+      max-width: 200px;
+      align-items: center;
+      display: grid;
+      justify-content: center;
+      float: center;
+      font-size: 18px; 
+
+    background: linear-gradient(to right, #f9611a, #f40b93, #8d029c);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    color: transparent; 
+    }`]})};var y=class e{constructor(o){this.sanitizer=o}transform(o){return this.sanitizer.bypassSecurityTrustResourceUrl(o)}static \u0275fac=function(n){return new(n||e)(B(R,16))};static \u0275pipe=I({name:"safeUrl",type:e,pure:!0})};function Z(e,o){if(e&1&&(t(0,"div",5),a(1,"iframe",6),D(2,"safeUrl"),t(3,"p"),r(4),i()()),e&2){let n=o.$implicit;c(),s("src",z(2,2,n.url),F),c(3),x(n.name)}}var M=class e{videos=[{name:"Gopuram",url:"https://www.youtube.com/embed/1Kx1WZsXcVA"},{name:"babyKrishna",url:"https://www.youtube.com/embed/mBm_CE_V48w"},{name:"homeSweetHome",url:"https://www.youtube.com/embed/EFYjA9snZBI"},{name:"Budha",url:"https://www.youtube.com/embed/_cxpbcyLQ-I"}];static \u0275fac=function(n){return new(n||e)};static \u0275cmp=l({type:e,selectors:[["app-paintingcomponent"]],decls:8,vars:1,consts:[[1,"gallery"],["class","video-item",4,"ngFor","ngForOf"],[1,"home"],["routerLink","/"],["routerLink","/page1"],[1,"video-item"],["width","360","height","115","frameborder","0","allowfullscreen","",3,"src"]],template:function(n,u){n&1&&(t(0,"body")(1,"div",0),b(2,Z,5,4,"div",1),i(),t(3,"div",2)(4,"a",3),r(5,"Home"),i(),t(6,"a",4),r(7,"Explore painting Catalog"),i()()()),n&2&&(c(2),s("ngForOf",u.videos))},dependencies:[v,g,y],styles:[".gallery[_ngcontent-%COMP%]{display:flex;flex-wrap:wrap;gap:15px}.video-item[_ngcontent-%COMP%]{text-align:center;border-radius:5px;padding:10px;width:350px;height:180px;border-style:double;border-color:#e7caab;border-width:10px}img[_ngcontent-%COMP%]{max-width:100%;border-radius:5px}body[_ngcontent-%COMP%]{background-image:linear-gradient(to right,#e9e8e6,#e7caab)}",`.home[_ngcontent-%COMP%] {
+      max-width: 5.2rem;
+      max-width: 1200px;
+      justify-content: center;
+      display: grid;
+      align-items: center;
+      float: center;
+      margin-top: 100px;
+      background-image: linear-gradient(to left,#e6d9ca,#E7CAAB);
+    }
+    a[_ngcontent-%COMP%] {
+      max-width: 5.2rem;
+      max-width: 200px;
+      align-items: center;
+      display: grid;
+      justify-content: center;
+      float: center;
+      background: linear-gradient(to right, #f9611a, #f40b93, #8d029c);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    color: transparent; 
+    }
+    p[_ngcontent-%COMP%]{
+        background: linear-gradient(to right, #f9611a, #f40b93, #8d029c);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    color: transparent; 
+    }`]})};var k=class e{static \u0275fac=function(n){return new(n||e)};static \u0275cmp=l({type:e,selectors:[["app-homecomponent"]],decls:37,vars:0,consts:[[1,"main"],[1,"content"],[1,"left-side"],["role","separator","aria-label","Divider",1,"divider"],[1,"right-side"],[1,"pill-group"],["routerLink","/page1"],["routerLink","/page2"],[1,"social-links"],["href","https://github.com/Raghavi-Mohan","aria-label","Github","target","_blank","rel","noopener"],["width","25","height","24","viewBox","0 0 25 24","fill","none","xmlns","http://www.w3.org/2000/svg","alt","Github"],["d","M12.3047 0C5.50634 0 0 5.50942 0 12.3047C0 17.7423 3.52529 22.3535 8.41332 23.9787C9.02856 24.0946 9.25414 23.7142 9.25414 23.3871C9.25414 23.0949 9.24389 22.3207 9.23876 21.2953C5.81601 22.0377 5.09414 19.6444 5.09414 19.6444C4.53427 18.2243 3.72524 17.8449 3.72524 17.8449C2.61064 17.082 3.81137 17.0973 3.81137 17.0973C5.04697 17.1835 5.69604 18.3647 5.69604 18.3647C6.79321 20.2463 8.57636 19.7029 9.27978 19.3881C9.39052 18.5924 9.70736 18.0499 10.0591 17.7423C7.32641 17.4347 4.45429 16.3765 4.45429 11.6618C4.45429 10.3185 4.9311 9.22133 5.72065 8.36C5.58222 8.04931 5.16694 6.79833 5.82831 5.10337C5.82831 5.10337 6.85883 4.77319 9.2121 6.36459C10.1965 6.09082 11.2424 5.95546 12.2883 5.94931C13.3342 5.95546 14.3801 6.09082 15.3644 6.36459C17.7023 4.77319 18.7328 5.10337 18.7328 5.10337C19.3942 6.79833 18.9789 8.04931 18.8559 8.36C19.6403 9.22133 20.1171 10.3185 20.1171 11.6618C20.1171 16.3888 17.2409 17.4296 14.5031 17.7321C14.9338 18.1012 15.3337 18.8559 15.3337 20.0084C15.3337 21.6552 15.3183 22.978 15.3183 23.3779C15.3183 23.7009 15.5336 24.0854 16.1642 23.9623C21.0871 22.3484 24.6094 17.7341 24.6094 12.3047C24.6094 5.50942 19.0999 0 12.3047 0Z"],["href","https://Instagram.com/texturedcanvaspainting/profilecard/?igsh=M203MmpnNmRsZG91","aria-label","Instagram","target","_blank","rel","noopener"],["width","24","height","22","viewBox","0 0 20 16","fill","none","xmlns","http://www.w3.org/2000/svg","alt","Instagram"],["d","M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"],["href","https://www.youtube.com/@raghavim9761","aria-label","Youtube","target","_blank","rel","noopener"],["width","29","height","20","viewBox","0 0 29 20","fill","none","xmlns","http://www.w3.org/2000/svg","alt","Youtube"],["fill-rule","evenodd","clip-rule","evenodd","d","M27.4896 1.52422C27.9301 1.96749 28.2463 2.51866 28.4068 3.12258C29.0004 5.35161 29.0004 10 29.0004 10C29.0004 10 29.0004 14.6484 28.4068 16.8774C28.2463 17.4813 27.9301 18.0325 27.4896 18.4758C27.0492 18.9191 26.5 19.2389 25.8972 19.4032C23.6778 20 14.8068 20 14.8068 20C14.8068 20 5.93586 20 3.71651 19.4032C3.11363 19.2389 2.56449 18.9191 2.12405 18.4758C1.68361 18.0325 1.36732 17.4813 1.20683 16.8774C0.613281 14.6484 0.613281 10 0.613281 10C0.613281 10 0.613281 5.35161 1.20683 3.12258C1.36732 2.51866 1.68361 1.96749 2.12405 1.52422C2.56449 1.08095 3.11363 0.76113 3.71651 0.596774C5.93586 0 14.8068 0 14.8068 0C14.8068 0 23.6778 0 25.8972 0.596774C26.5 0.76113 27.0492 1.08095 27.4896 1.52422ZM19.3229 10L11.9036 5.77905V14.221L19.3229 10Z"],[1,"container"],[1,"centered-div"],[1,"image-container"],["width","280","height","200","src","brandImage.png","alt","Description",1,"img-responsive"]],template:function(n,u){n&1&&(t(0,"body")(1,"main",0)(2,"div",1)(3,"div",2)(4,"h1"),r(5,"Hello From, TextureAndTones"),i(),t(6,"p"),r(7,"Welcome to our vibrant corner where creativity and coding converge!Explore the captivating world of canvas texture paintings, where artistic expression meets innovative coding techniques !!"),i()(),a(8,"div",3),t(9,"div",4)(10,"div",5)(11,"nav")(12,"ul")(13,"li")(14,"a",6),r(15,"Explore Collection"),i()(),t(16,"li")(17,"a",7),r(18,"Explore making videos"),i()()()()(),t(19,"div",8)(20,"a",9),h(),t(21,"svg",10),a(22,"path",11),i()(),C(),t(23,"a",12),h(),t(24,"svg",13),a(25,"path",14),r(26," /> "),i()(),C(),t(27,"a",15),h(),t(28,"svg",16),a(29,"path",17),i()()()()()(),C(),t(30,"div",18)(31,"div",19)(32,"h1"),r(33,"Please checkout my paintings!!"),i()(),t(34,"div",20),a(35,"br")(36,"img",21),i()()())},dependencies:[g],styles:["body[_ngcontent-%COMP%]{background-image:linear-gradient(to right,#f0efee,#e7caab)}",`[_nghost-%COMP%] {
+      --bright-blue: oklch(51.01% 0.274 263.83);
+      --electric-violet: oklch(53.18% 0.28 296.97);
+      --french-violet: oklch(47.66% 0.246 305.88);
+      --vivid-pink: oklch(69.02% 0.277 332.77);
+      --hot-red: oklch(61.42% 0.238 15.34);
+      --orange-red: oklch(63.32% 0.24 31.68);
+  
+      --gray-900: oklch(19.37% 0.006 300.98);
+      --gray-700: oklch(36.98% 0.014 302.71);
+      --gray-400: oklch(70.9% 0.015 304.04);
+  
+      --red-to-pink-to-purple-vertical-gradient: linear-gradient(
+        180deg,
+        var(--orange-red) 0%,
+        var(--vivid-pink) 50%,
+        var(--electric-violet) 100%
+      );
+  
+      --red-to-pink-to-purple-horizontal-gradient: linear-gradient(
+        90deg,
+        var(--orange-red) 0%,
+        var(--vivid-pink) 50%,
+        var(--electric-violet) 100%
+      );
+  
+      --pill-accent: var(--bright-blue);
+  
+      font-family: "Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+        Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+        "Segoe UI Symbol";
+      box-sizing: border-box;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+    }
+  
+    h1[_ngcontent-%COMP%] {
+      font-size: 3.125rem;
+      color: var(--gray-900);
+      font-weight: 500;
+      line-height: 100%;
+      letter-spacing: -0.125rem;
+      margin: 0;
+      font-family: "Inter Tight", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+        Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+        "Segoe UI Symbol";
+        background: linear-gradient(to right, #f9611a, #f40b93, #8d029c);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    color: transparent; 
+    }
+  
+    p[_ngcontent-%COMP%] {
+      margin: 0;
+      color: var(--gray-700);
+      background: linear-gradient(to right, #f9611a, #f40b93, #8d029c);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    color: transparent; 
+    }
+
+    a[_ngcontent-%COMP%]{
+        background: linear-gradient(to right, #f9611a, #f40b93, #8d029c);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    color: transparent; 
+    }
+  
+    main[_ngcontent-%COMP%] {
+      width: 100%;
+      min-height: 80%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding-bottom: 5.5rem;
+      box-sizing: inherit;
+      position: relative;
+    }
+  
+    .img-responsive[_ngcontent-%COMP%] {
+      max-width: 9.2rem;
+      max-width: 400px;
+      align-items: center;
+      float: center;
+      border-radius: 5px;
+      border-style:double;
+      border-color: #E7CAAB;
+      border-width: 10px;
+
+    }
+  
+    .content[_ngcontent-%COMP%] {
+      display: flex;
+      justify-content: space-around;
+      width: 100%;
+      max-width: 1100px;
+    }
+  
+    .content[_ngcontent-%COMP%]   h1[_ngcontent-%COMP%] {
+      margin-top: 1.75rem;
+    }
+  
+    .content[_ngcontent-%COMP%]   p[_ngcontent-%COMP%] {
+      margin-top: 1.5rem;
+    }
+  
+    .divider[_ngcontent-%COMP%] {
+      width: 3px;
+      background: var(--red-to-pink-to-purple-vertical-gradient);
+      margin-inline: 0.5rem;
+      height: 180px;
+    }
+  
+    .pill-group[_ngcontent-%COMP%] {
+      display: flex;
+      flex-direction: column;
+      align-items: start;
+      flex-wrap: wrap;
+      gap: 1.25rem;
+    }
+    .left-side[_ngcontent-%COMP%]{
+      background-image: linear-gradient(to left,#e9e0d7,#E7CAAB);
+    }
+  
+    .pill[_ngcontent-%COMP%] {
+      display: flex;
+      align-items: center;
+      --pill-accent: var(--bright-blue);
+      background: color-mix(in srgb, var(--pill-accent) 5%, transparent);
+      color: var(--pill-accent);
+      padding-inline: 0.75rem;
+      padding-block: 0.375rem;
+      border-radius: 2.75rem;
+      border: 0;
+      transition: background 0.3s ease;
+      font-family: var(--inter-font);
+      font-size: 0.875rem;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 1.4rem;
+      letter-spacing: -0.00875rem;
+      text-decoration: none;
+    }
+  
+    .pill[_ngcontent-%COMP%]:hover {
+      background: color-mix(in srgb, var(--pill-accent) 15%, transparent);
+    }
+  
+    .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 1) {
+      --pill-accent: var(--bright-blue);
+    }
+    .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 2) {
+      --pill-accent: var(--french-violet);
+    }
+    .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 3), 
+   .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 4), 
+   .pill-group[_ngcontent-%COMP%]   .pill[_ngcontent-%COMP%]:nth-child(6n + 5) {
+      --pill-accent: var(--hot-red);
+    }
+  
+    .pill-group[_ngcontent-%COMP%]   svg[_ngcontent-%COMP%] {
+      margin-inline-start: 0.25rem;
+    }
+  
+    .social-links[_ngcontent-%COMP%] {
+      display: flex;
+      align-items: center;
+      gap: 0.73rem;
+      margin-top: 1.5rem;
+    }
+  
+    .social-links[_ngcontent-%COMP%]   path[_ngcontent-%COMP%] {
+      transition: fill 0.3s ease;
+      fill: var(--gray-400);
+    }
+    .container[_ngcontent-%COMP%] {
+    display: grid;
+    bottom:100px;
+    width:100%;
+    right:25px;
+    align-items: center;
+    justify-content: center;
+    float: center; 
+
+    height: 10vh; 
+
+    background-image: linear-gradient(to left,#e6d9ca,#E7CAAB);
+  }
+
+  
+  .image-container[_ngcontent-%COMP%] {
+    display: grid;
+    bottom:50px;
+    right:25px;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    float: center; 
+
+    height: 10vh; 
+
+  }
+  
+    .social-links[_ngcontent-%COMP%]   a[_ngcontent-%COMP%]:hover   svg[_ngcontent-%COMP%]   path[_ngcontent-%COMP%] {
+      fill: var(--gray-900);
+    }
+  
+    @media screen and (max-width: 650px) {
+      .content[_ngcontent-%COMP%] {
+        flex-direction: column;
+        width: max-content;
+      }
+  
+      .divider[_ngcontent-%COMP%] {
+        height: 1px;
+        width: 100%;
+        background: var(--red-to-pink-to-purple-horizontal-gradient);
+        margin-block: 1.5rem;
+      }
+    }`]})};var K=[{path:"",component:k},{path:"page1",component:w},{path:"page2",component:M},{path:"**",redirectTo:""}],P=class e{static \u0275fac=function(n){return new(n||e)};static \u0275mod=d({type:e});static \u0275inj=p({imports:[O.forRoot(K),O]})};var f=class e{title="my-artApp";static \u0275fac=function(n){return new(n||e)};static \u0275cmp=l({type:e,selectors:[["app-root"]],decls:1,vars:0,template:function(n,u){n&1&&a(0,"router-outlet")},dependencies:[U]})};var _=class e{static \u0275fac=function(n){return new(n||e)};static \u0275mod=d({type:e,bootstrap:[f]});static \u0275inj=p({providers:[T()],imports:[H,P]})};var A=class e{static \u0275fac=function(n){return new(n||e)};static \u0275mod=d({type:e,bootstrap:[f]});static \u0275inj=p({imports:[_,L]})};export{A as a};
