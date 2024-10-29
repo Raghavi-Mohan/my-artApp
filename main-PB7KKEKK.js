02a6595d8d2c9bc00495115865d1c5803cb65f4e
Raghavi Mohan <70186244+Raghavi-Mohan@users.noreply.github.com>
2024-10-28 22:57:41 -0400

    Update main-PB7KKEKK.js

--- main-PB7KKEKK.js
+++ main-PB7KKEKK.js
@@ -270,7 +270,7 @@ ${t.map((r,o)=>`${o+1}) ${r.toString()}`).join(`
     align-items: center;
     justify-content: center;
     float: center; 
-    height: 150px;
+    height: 100px;
     background-image: linear-gradient(to left,#e6d9ca,#E7CAAB);
   }
 
